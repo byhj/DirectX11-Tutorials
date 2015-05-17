e8507dbb24a2759c25056427aa79fb7212655ba2
byhj <476652422@qq.com>
2015-05-17 15:52:41 +0800

    Add font

---
+++ ch12-Font-Engine/FontShader.h
@@ -0,0 +1,52 @@
+#ifndef  FONTSHADER_H
+#define  FONTSHADER_H
+
+#include <d3d11.h>
+#include <D3DX10math.h>
+#include <D3DX11async.h>
+#include <fstream>
+
+
+class  FontShader
+{
+public:
+	FontShader();
+	FontShader(const FontShader &);
+	~ FontShader();
+
+	bool Init(ID3D11Device *, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX, ID3D11ShaderResourceView*, D3DXVECTOR4);
+
+private:
+	bool InitShader(ID3D11Device *, HWND, WCHAR *, WCHAR * );
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob *, HWND, WCHAR *);
+	bool SetShaderParameters(ID3D11DeviceContext*, D3DXMATRIX, D3DXMATRIX, D3DXMATRIX, ID3D11ShaderResourceView*, D3DXVECTOR4);
+
+	void RenderShader(ID3D11DeviceContext*, int);
+
+	struct MatrixBuffer
+	{
+		D3DXMATRIX World;
+		D3DXMATRIX View;
+		D3DXMATRIX Proj;
+	};
+
+	struct  PixelBuffer
+	{
+		D3DXVECTOR4 pixelColor;
+
+	};
+
+private:
+	ID3D11VertexShader *pVS_Shader;
+	ID3D11PixelShader  *pPS_Shader;
+	ID3D11InputLayout  *pInputLayout;
+	ID3D11Buffer       *pMatrixBuffer;
+	ID3D11SamplerState *pSampleState;
+    ID3D11Buffer       *pPixelBuffer;
+};
+
+
+#endif // ! BitMapShader_H
---
+++ ch12-Font-Engine/TextClass.h
@@ -0,0 +1,55 @@
+#ifndef TextCLASS_H
+#define TextCLASS_H
+
+#include <d3d11.h>
+#include <d3dx10math.h>
+
+#include "TextureClass.h"
+#include "FontClass.h"
+#include "FontShader.h"
+
+class TextClass
+{
+private:
+	struct Vertex
+	{
+		D3DXVECTOR3 Pos;
+		D3DXVECTOR2 Tex;
+	};
+	struct Sentence
+	{
+		ID3D11Buffer *vertexBuffer, *indexBuffer;
+		int vertexCount, indexCount, maxLength;
+		float red, green, blue;
+	};
+
+public:
+	TextClass();
+	TextClass(const TextClass &);
+	~TextClass();
+
+	bool Init(ID3D11Device *, ID3D11DeviceContext *, HWND, int, int, D3DXMATRIX);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext *, D3DXMATRIX, D3DXMATRIX);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView *GetTexture();
+
+private:
+	bool InitBuffer(Sentence**, int, ID3D11Device*);
+	void ShutdownBuffer(Sentence **);
+	bool UpdateBuffer(Sentence *, char *, int, int, float, float, float, ID3D11DeviceContext*);
+	bool RenderBuffer(ID3D11DeviceContext *, Sentence *, D3DXMATRIX, D3DXMATRIX);
+
+private:
+    FontClass *pFont;
+    FontShader *pFontShader;
+	int screenWidth, screenHeight;
+	D3DXMATRIX BaseView;
+
+	Sentence *pSentence1;
+	Sentence *pSentence2;
+};
+
+
+#endif
