508855103dd0e90db0c6104fb384f1423c950525
byhj <476652422@qq.com>
2015-05-18 11:34:32 +0800

    Add texture array

---
+++ ch17-Texture-Array/texture-vs.hlsl
@@ -0,0 +1,34 @@
+
+cbuffer MatrixBuffer
+{
+	matrix World;
+	matrix View;
+	matrix Proj;
+
+};
+
+struct VS_IN
+{
+	 float4 Position: POSITION;
+	 float2 Tex : TEXCOORD0;
+};
+
+struct VS_OUT
+{
+	float4 Position: SV_POSITION;
+	float2 Tex :TEXCOORD0;
+};
+
+VS_OUT VS(VS_IN vs_in)
+{
+	VS_OUT vs_out;
+	vs_in.Position.w = 1.0f;
+	vs_out.Position = mul(vs_in.Position, World);
+	vs_out.Position = mul(vs_out.Position, View);
+	vs_out.Position = mul(vs_out.Position, Proj);
+
+	vs_out.Tex = vs_in.Tex;
+
+	return vs_out;
+}
+
