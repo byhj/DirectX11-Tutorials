5d66dd9ae0545500463985404d47d653e16299c8
binyu <binyuapple@163.com>
2015-05-19 23:07:38 +0800

    Add clipping & texture translation

---
+++ ch25-Texture-Translation/GraphicsClass.cpp
@@ -0,0 +1,195 @@
+#include "graphicsclass.h"
+
+GraphicsClass::GraphicsClass()
+	:pD3D(0),
+	 pCamera(0),
+	 pModel(0),
+	 pShader(0),
+	 pLight(0)
+{
+}
+
+
+GraphicsClass::GraphicsClass(const GraphicsClass& rhs)
+{
+}
+
+
+GraphicsClass::~GraphicsClass()
+{
+}
+
+
+bool GraphicsClass::Init(int screenWidth, int screenHeight, HWND hWnd)
+{
+	bool result;
+
+	/////////////////////// D3DClass /////////////////////////////////////
+
+	pD3D = new D3DClass;
+	if(!pD3D)
+	{
+		return false;
+	}
+
+	result = pD3D->Init(screenWidth, screenHeight, VSYNC_ENABLED, hWnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
+	if(!result)
+	{
+		MessageBox(hWnd, L"Could not initialize Direct3D", L"Error", MB_OK);
+		return false;
+	}
+
+	////////////////////////Camera  Class///////////////////////////
+
+	pCamera = new CameraClass;
+	if (!pCamera)
+	{
+		return false;
+	}
+	pCamera->SetPosition(0.0f, 0.0f, -5.0f);
+
+	//////////////////////// Model Class ////////////////////////////
+	pModel = new ModelClass;
+	if (!pModel)
+	{
+		return false;
+	}
+
+	result = pModel->Init(pD3D->GetDevice(), "cube.txt", L"../common/media/texture/stone02.dds", 
+		L"../common/media/texture/bump02.dds", L"../common/media/texture/spec02.dds");
+	
+	if(!result)
+	{
+		MessageBox(hWnd, L"Could not initialize the model object.", L"Error", MB_OK);
+		return false;
+	}
+
+	///////////////////////Shader Class ////////////////////////////
+	pShader = new ShaderClass;
+	if (!pShader)
+	{
+       return false;
+	}
+
+	result = pShader->Init(pD3D->GetDevice(), hWnd);
+	if(!result)
+	{
+		MessageBox(hWnd, L"Could not initialize the color shader object.", L"Error", MB_OK);
+		return false;
+	}
+	///////////////////////////////////////////////////////////////////
+
+	pLight = new LightClass;
+	if (!pLight)
+	{
+		return false;
+	}
+
+	pLight->SetDiffuseColor(1.0f, 1.0f, 1.0f, 1.0f);
+	pLight->SetDirection(0.0f, 0.0f, 1.0f);
+	pLight->SetSpecularColor(1.0f, 1.0f, 1.0f, 1.0f);
+	pLight->SetSpecularPower(16.0f);
+
+	return true;
+}
+
+
+void GraphicsClass::Shutdown()
+{
+	if (pLight)
+	{
+		delete pLight;
+		pLight = 0;
+	}
+
+	// Release the color shader object.
+	if(pShader)
+	{
+		pShader->Shutdown();
+		delete pShader;
+		pShader = 0;
+	}
+
+	// Release the model object.
+	if(pModel)
+	{
+		pModel->Shutdown();
+		delete pModel;
+		pModel = 0;
+	}
+
+	// Release the camera object.
+	if(pCamera)
+	{
+		delete pCamera;
+		pCamera = 0;
+	}
+
+	//Release d3d object
+	if(pD3D)
+	{
+		pD3D->Shutdown();
+		delete pD3D;
+		pD3D = 0;
+	}
+	return;
+}
+
+
+bool GraphicsClass::Render()
+{
+	bool result;
+
+	static float rot = 0.0f;
+	rot += (float)D3DX_PI * 0.00005f;
+	if (rot > 360.0f)
+	{
+		rot = 0.0f;
+	}
+	result = RenderGraphics(rot);
+
+	if(!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+bool GraphicsClass::RenderGraphics(float rot)
+{
+	D3DXMATRIX View, Proj, World;
+	bool result;
+
+	static float textureTranslation = 0.0f;
+	textureTranslation += 0.01f;
+	if(textureTranslation > 1.0f)
+	{
+		textureTranslation -= 1.0f;
+	}
+
+	pD3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
+    pCamera->Render();
+
+	pCamera->GetViewMatrix(View);
+	pD3D->GetWorld(World);
+	pD3D->GetProj(Proj);
+	pModel->Render( pD3D->GetDeviceContext() );
+
+	//D3DXMatrixRotationX(&World, rot);
+	D3DXMatrixRotationY(&World, rot);
+	//D3DXMatrixRotationZ(&World, rot);
+
+	//Render the Model Using Shader
+	result = pShader->Render(pD3D->GetDeviceContext(), pModel->GetIndexCount(), World, View, Proj, 
+		                     pModel->GetTextureArray(), pLight->GetDirection(), pLight->GetDiffuseColor(),
+							 pCamera->GetPosition(), pLight->GetSpecularColor(), pLight->GetSpecularPower(),textureTranslation);
+    if (!result)
+	{
+		return false;
+	}
+
+	pD3D->EndScene();
+	return true;
+}
---
+++ ch25-Texture-Translation/texture-fs.hlsl
@@ -0,0 +1,82 @@
+
+struct VS_OUT
+{
+	 float4 Position  :SV_POSITION;
+	 float2 Tex       :TEXCOORD0;
+	 float3 normal    :NORMAL;
+	 float3 tangent   :TANGENT;
+	 float3 binormal  :BINORMAL;
+	 float3 viewDirection :TEXCOORD1;
+};
+
+cbuffer LightBuffer
+{
+	float4 diffuseColor;
+	float4 specularColor;
+	float specularPower;
+	float3 lightDirection;
+};
+
+cbuffer TranslationBuffer
+{
+    float textureTranslation;
+};
+
+
+Texture2D shaderTextures[3];
+SamplerState SampleType;
+
+float4 PS(VS_OUT vs_out): SV_TARGET
+{
+    float4 textureColor;
+    float4 bumpMap;
+    float3 bumpNormal;
+    float3 lightDir;
+    float  lightIntensity;
+    float4 color;
+    float4 specularIntensity;
+    float3 reflection;
+    float4 specular;
+	vs_out.Tex.x += textureTranslation;
+
+    textureColor = shaderTextures[0].Sample(SampleType, vs_out.Tex);
+    bumpMap = shaderTextures[1].Sample(SampleType, vs_out.Tex);
+
+    // Expand the range of the normal value from (0, +1) to (-1, +1).
+    bumpMap = (bumpMap * 2.0f) - 1.0f;
+
+    // Calculate the normal from the data in the bump map.
+    bumpNormal = (bumpMap.x * vs_out.tangent) + (bumpMap.y * vs_out.binormal) + (bumpMap.z * vs_out.normal);
+	
+    // Normalize the resulting bump normal.
+    bumpNormal = normalize(bumpNormal);
+
+    // Invert the light direction for calculations.
+    lightDir = -lightDirection;
+
+    // Calculate the amount of light on this pixel based on the bump map normal value.
+    lightIntensity = saturate(dot(bumpNormal, lightDir));
+
+    // Determine the final diffuse color based on the diffuse color and the amount of light intensity.
+    color = saturate(diffuseColor * lightIntensity);
+
+    // Combine the final bump light color with the texture color.
+    color = color * textureColor;
+
+	if(lightIntensity > 0.0f)
+    {        // Sample the pixel from the specular map texture.
+        specularIntensity = shaderTextures[2].Sample(SampleType, vs_out.Tex);
+	        // Calculate the reflection vector based on the light intensity, normal vector, and light direction.
+        reflection = normalize(2 * lightIntensity * bumpNormal - lightDir); 
+
+        // Determine the amount of specular light based on the reflection vector, viewing direction, and specular power.
+        specular = pow(saturate( dot(reflection, vs_out.viewDirection) ), specularPower);
+		        // Use the specular map to determine the intensity of specular light at this pixel.
+        specular = specular * specularIntensity;
+		
+        // Add the specular component last to the output color.
+        color = saturate(color + specular);
+    }
+		
+	return color;
+}
