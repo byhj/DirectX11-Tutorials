482f4ad0d5325858085b4633cd9f460a211cdbc8
binyu <binyuapple@163.com>
2015-05-23 16:58:39 +0800

    Add

---
+++ ch29-Water/ModelClass.h
@@ -0,0 +1,82 @@
+#ifndef MODELCLASS_H
+#define MODELCLASS_H
+
+#include <d3d11.h>
+#include <D3DX10math.h>
+
+#include "TextureClass.h"
+
+#include <fstream>
+
+class ModelClass 
+{
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass &);
+	~ModelClass();
+
+	bool Init(ID3D11Device *, char *, WCHAR *, WCHAR *, WCHAR *);
+	void Shutdown();
+	void Render(ID3D11DeviceContext *);
+
+	int GetIndexCount();
+	//pass texture resource to shaders
+	ID3D11ShaderResourceView **GetTextureArray();
+	ID3D11ShaderResourceView *GetTexture();
+private:
+	struct VertexType
+	{
+		D3DXVECTOR3 position;
+		D3DXVECTOR2 texture;
+		D3DXVECTOR3 normal;
+		D3DXVECTOR3 tangent;
+		D3DXVECTOR3 binormal;
+	};
+
+	struct ModelType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+		float tx, ty, tz;
+		float bx, by, bz;
+	};
+
+	struct TempVertexType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
+	struct VectorType
+	{
+		float x, y, z;
+	};
+
+private:
+	bool InitBuffer(ID3D11Device *);
+	void ShutdownBuffer();
+	void RenderBuffer(ID3D11DeviceContext *);
+	bool LoadTextures(ID3D11Device*, WCHAR *, WCHAR *, WCHAR *);
+	void ShutdownTexture();
+
+	bool LoadModel(char *);
+	void ShoutdownModel();
+
+	void CalculateModelVectors();
+	void CalculateTangentBinormal(TempVertexType, TempVertexType, TempVertexType, VectorType&, VectorType&);
+	void CalculateNormal(VectorType, VectorType, VectorType&);
+
+private:
+	ID3D11Buffer *pVertexBuffer;
+	ID3D11Buffer *pIndexBuffer;
+	TextureClass *pTexture;	
+	ModelType  *pModelVertex;
+
+	int VertexCount, IndexCount;
+};
+
+#endif
+
