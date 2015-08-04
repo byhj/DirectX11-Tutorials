56a8bb7a0f8e422b39ff9c13fc23b90b7ea70743
byhj <476652422@qq.com>
2015-08-04 12:09:30 +0800

    modifed ch1-10

---
+++ src/ch10-Specular-Light/triangle.h
@@ -0,0 +1,76 @@
+#ifndef Triangle_H
+#define Triangle_H
+
+#include <windows.h>
+#include <xnamath.h>
+#include <vector>
+
+#include "d3d/d3dShader.h"
+#include "d3d/d3dUtility.h"
+
+
+namespace byhj
+{
+
+
+class Triangle
+{
+public:
+	Triangle();
+	~Triangle();
+
+	void Init(ID3D11Device *pD3D11Device, ID3D11DeviceContext *pD3D11DeviceContext, HWND hWnd);
+	void Render(ID3D11DeviceContext *pD3D11DeviceContext, const MatrixBuffer &matrix);
+	void Shutdown();
+
+private:
+	void init_buffer(ID3D11Device *pD3D11Device, ID3D11DeviceContext *pD3D11DeviceContext);
+	void init_shader(ID3D11Device *pD3D11Device, HWND hWnd);
+	void init_texture(ID3D11Device *pD3D11Device);
+	void load_model(char *modelFile);
+
+	struct LightBuffer
+	{
+		XMFLOAT4 ambientColor;
+		XMFLOAT4 diffuseColor;
+		XMFLOAT3 lightDirection;
+		float specularPower;
+		XMFLOAT4 specularColor;
+	};
+	LightBuffer cbLight;
+
+	struct CameraBuffer
+	{
+		XMFLOAT3 camPos;
+		float padding;
+	};
+
+	struct  Vertex
+	{
+		XMFLOAT3 Position;
+		XMFLOAT2 TexCoord;
+		XMFLOAT3 Normal;
+	};
+   
+	MatrixBuffer cbMatrix;
+	ID3D11InputLayout        *m_pInputLayout      = nullptr;
+	ID3D11VertexShader       *m_pVS               = nullptr;
+	ID3D11PixelShader        *m_pPS               = nullptr;
+	ID3D11Buffer             *m_pMVPBuffer        = nullptr;
+	ID3D11Buffer             *m_pVertexBuffer     = nullptr;
+	ID3D11Buffer             *m_pIndexBuffer      = nullptr;
+	ID3D11Buffer             *m_pLightBuffer      = nullptr;
+	ID3D11Buffer             *m_CameraBuffer      = nullptr;
+	ID3D11ShaderResourceView *m_pTexture          = nullptr;
+	ID3D11SamplerState       *m_pTexSamplerState  = nullptr;
+	byhj::Shader TriangleShader;
+
+	std::vector<Vertex> m_VertexData;
+	std::vector<DWORD> m_IndexData;
+};
+
+
+}
+
+
+#endif 
